85b1c4598b153f77a006be408db2b1d8a38180d0
OR_TH <90402282+OneRider12@users.noreply.github.com>
2025-11-03 17:39:37 +0700

    for merging 2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 
+from game.GamePage import GamePage
 from home.HomePage import HomePage
 
 class GameInstance:
     def __init__(self):
         pygame.init()
-        self.instance = HomePage()
+        self.instance = GamePage()
         self.instance.run()
 
 if __name__ == "__main__":
