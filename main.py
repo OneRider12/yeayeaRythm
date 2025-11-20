380ed492cdfe18e02dac8ce8e68e129fe7e96edc
OR_TH <90402282+OneRider12@users.noreply.github.com>
2025-11-20 09:46:46 +0700

    some strange

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-        pygame.init()
+import pygame, sys
+import Start
+import Setting
+
+pygame.init()
+WIDTH, HEIGHT = 1324, 768
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("YeaYea RHYTHM")
+
+clock = pygame.time.Clock()
+current_scene = "start"   # เริ่มต้นที่หน้า start
+
+# -------- Main loop --------
+while True:
+    dt = clock.tick(60) / 1000.0
+
+    if current_scene == "start":
+        next_scene = Start.run(screen, dt)
+        if next_scene:
+            current_scene = next_scene
+
+    elif current_scene == "setting":
+        next_scene = Setting.run(screen, dt)
+        if next_scene:
+            current_scene = next_scene
+
+    # ปิดเกม
+    if current_scene == "quit":
+        pygame.quit()
+        sys.exit()
+
